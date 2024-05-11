37bdce026760f6e6d413420754588e34465968f7
aksarlaode <akhsaras21@gmail.com>
2024-05-11 14:56:45 +0000

    component

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import "@blocknote/core/fonts/inter.css";
@@ -6,42 +6,10 @@ import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/shadcn";
 import "@blocknote/shadcn/style.css";
 
-import * as Button from "@/components/ui/button";
-import * as Form from "@/components/ui/form";
-import * as Badge from "@/components/ui/badge";
-import * as Card from "@/components/ui/card";
-import * as DropdownMenu from "@/components/ui/dropdown-menu";
-import * as Input from "@/components/ui/input";
-import * as Label from "@/components/ui/label";
-import * as Popover from "@/components/ui/popover";
-import * as Select from "@/components/ui/select";
-import * as Tabs from "@/components/ui/tabs";
-import * as Toggle from "@/components/ui/toggle";
-import * as Tooltip from "@/components/ui/tooltip";
-
 function Editor() {
   const editor = useCreateBlockNote();
 
-  // Renders the editor instance using a React component.
-  return (
-    <BlockNoteView
-      editor={editor}
-      shadCNComponents={{
-        Button,
-        Form,
-        Badge,
-        Card,
-        DropdownMenu,
-        Input,
-        Label,
-        Popover,
-        Select,
-        Tabs,
-        Toggle,
-        Tooltip,
-      }}
-    />
-  );
+  return <BlockNoteView editor={editor} />;
 }
 
 export default Editor;
